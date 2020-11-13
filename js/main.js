c6a5aa7b52155bdf6964e387e9b72fd6f6cbcbea
screen <bao.future94@gmail.com>
2020-11-13 02:18:01 -0800

    aaa

---
+++ js/main.js
@@ -0,0 +1,43 @@
+App = {};
+(function ($) {
+    App.myFunction = () => {
+        console.log('a')
+    }
+
+    // The functions App
+    // Resize screen and addClass/removeClass of menu
+    App.checkWindowSize = () => {
+        var width = $(window).width(),
+            new_class = width > 1200 ? 'menu-desk' : 'menu-mobile';
+
+        $('header').removeClass('menu-desk menu-mobile').addClass(new_class);
+    }
+
+
+    $('.button-bar').on('click', function () {
+        $('body .main-nav').toggleClass('active');
+        $('.bg-pushmenu').addClass('in')
+    });
+
+    $('.bg-pushmenu').on('click', function() {
+        $(this).removeClass('in');
+        $('body .main-nav').removeClass('active');
+    })
+
+    // App.closeMenu = () => {
+    //     $('.bg-menu').on('click', function() {
+    //         $('body .main-nav').removeClass('active');
+    //         $(this).remove();
+    //     })
+    // }
+    // When resize screen
+    $(window).resize(function () {
+        App.checkWindowSize();
+    });
+
+    // Run first
+    $(document).ready(function () {
+        App.checkWindowSize();
+    })
+
+})(jQuery);
